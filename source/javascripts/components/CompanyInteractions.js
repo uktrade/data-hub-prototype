71aae8a41681bac9290c36055e296302db386304
Zac Tolley <zac@thetolleys.com>
2016-04-13 16:35:05 +0100

    Change date format

--- source/javascripts/components/CompanyInteractions.js
+++ source/javascripts/components/CompanyInteractions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactLink from './ContactLink';
+import { Link } from 'react-router';
 import Time from 'react-time';
 
 class CompanyInteractions extends React.Component {
@@ -7,11 +8,11 @@ class CompanyInteractions extends React.Component {
   renderInteraction(interaction, i) {
     return (
       <li key={i} className="interaction">
-        <h2 className="heading interaction-heading">{interaction.type}</h2>
-        <div className="interaction-date"><Time value={interaction.date} format="DD.MM.YY" /></div>
-        <div className="interaction-description">{interaction.description}</div>
-        <div className="interaction-advisor">Advisor: <ContactLink data={interaction.advisor} /></div>
-        <div className="interaction-contact">Contact: <ContactLink data={interaction.contact} /></div>
+          <h2 className="heading interaction-heading">{interaction.type}</h2>
+          <div className="interaction-date"><Time value={interaction.date} format="DD MMM YY" /></div>
+          <div className="interaction-description">{interaction.description}</div>
+          <div className="interaction-advisor">Advisor: <ContactLink data={interaction.advisor} /></div>
+          <div className="interaction-contact">Contact: <ContactLink data={interaction.contact} /></div>
       </li>
     );
   }
