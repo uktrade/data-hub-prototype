f531e830155630c2c4f45b5fefea52b97d8d4ad8
Zac Tolley <zac@thetolleys.com>
2016-11-16 13:14:45 +0000

    re-enable authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,10 +2,10 @@
 
 module.exports = function auth(req, res, next) {
 
-  //if (req.url !== '/login' && req.url !== '/error' && !req.session.token) {
-  //  res.redirect('/login');
-  //  return;
-  //}
+  if (req.url !== '/login' && req.url !== '/error' && !req.session.token) {
+    res.redirect('/login');
+    return;
+  }
 
   next();
 
