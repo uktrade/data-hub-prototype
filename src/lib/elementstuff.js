91374ace97a67a60778210fba0337bdbf2d39f6b
Zac Tolley <zac@thetolleys.com>
2016-11-16 16:58:34 +0000

    Previous version of isNodelist didn’t work with IE (#205)

--- src/lib/elementstuff.js
+++ src/lib/elementstuff.js
@@ -56,9 +56,13 @@ function generateID() {
   });
 }
 
-function isNodeList(el) {
-  // Only NodeList has the "item()" function
-  return typeof el.item !== 'undefined';
+function isNodeList(nodes) {
+  const stringRepr = Object.prototype.toString.call(nodes);
+
+  return typeof nodes === 'object' &&
+    /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+    (typeof nodes.length === 'number') &&
+    (nodes.length === 0 || (typeof nodes[0] === 'object' && nodes[0].nodeType > 0));
 }
 
 module.exports = {
