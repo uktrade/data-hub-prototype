72cbfdf20c69fd1bcbcc05691bc6215c28330aa6
Zac Tolley <zac@thetolleys.com>
2016-11-18 15:46:59 +0000

    DHE-286 Fix display of advisor when editing interaction

--- src/controllers/interactioncontroller.js
+++ src/controllers/interactioncontroller.js
@@ -1,49 +1,48 @@
 /* eslint new-cap: 0 */
-'use strict';
-
 const express = require('express');
-const router = express.Router();
 const interactionRepository = require('../repositorys/interactionrepository');
 const contactRepository = require('../repositorys/contactrepository');
 const companyRepository = require('../repositorys/companyrepository');
-
 const controllerUtils = require('../lib/controllerutils');
 
+const router = express.Router();
+
 function view(req, res) {
-  let interaction_id = req.params.interaction_id;
+  const interaction_id = req.params.interaction_id;
 
   if (!interaction_id) {
     res.redirect('/');
   }
 
-  interactionRepository.getInteraction( req.session.token, interaction_id)
+  interactionRepository.getInteraction(req.session.token, interaction_id)
     .then((interaction) => {
       res.render('interaction/interaction-details', {
-        interaction
+        interaction,
       });
-
     })
     .catch((error) => {
-      res.render('error', {error});
+      res.render('error', { error });
     });
-
 }
 
 function edit(req, res) {
-  let interactionId = req.params.interaction_id || null;
+  const interactionId = req.params.interaction_id || null;
 
-  interactionRepository.getInteraction( req.session.token, interactionId)
+  interactionRepository.getInteraction(req.session.token, interactionId)
     .then((interaction) => {
+      if (interaction.dit_advisor && !interaction.name) {
+        interaction.dit_advisor.name = `${interaction.dit_advisor.first_name || ''} ${interaction.dit_advisor.last_name || ''}`;
+      }
+
       res.render('interaction/interaction-edit', {
         company: null,
         contact: null,
-        interaction: ( interaction || null )
+        interaction: (interaction || null),
       });
     });
 }
 
 function add(req, res) {
-
   const companyId = req.query.company_id;
   const contactId = req.query.contact_id;
 
@@ -54,7 +53,7 @@ function add(req, res) {
           company: contact.company,
           contact,
           interactionId: null,
-          interaction: null
+          interaction: null,
         });
       });
   } else if (companyId && companyId.length > 0) {
@@ -64,33 +63,31 @@ function add(req, res) {
           company,
           contact: null,
           interactionId: null,
-          interaction: null
+          interaction: null,
         });
       });
   } else {
     res.redirect('/');
   }
-
 }
 
 function post(req, res) {
-
-  let interaction = Object.assign({}, req.body.interaction);
+  const interaction = Object.assign({ }, req.body.interaction);
 
   controllerUtils.flattenIdFields(interaction);
   controllerUtils.nullEmptyFields(interaction);
 
   interactionRepository.saveInteraction(req.session.token, interaction)
-    .then(data => {
+    .then((data) => {
       res.json(data);
     })
-    .catch(error => {
+    .catch((error) => {
       if (typeof error.error === 'string') {
-        return res.status(error.response.statusCode).json({errors: [{'error': error.response.statusMessage}]});
+        return res.status(error.response.statusCode).json({ errors: [{ error: error.response.statusMessage }] });
       }
 
-      let errors = error.error;
-      return res.status(400).json({errors});
+      const errors = error.error;
+      return res.status(400).json({ errors });
     });
 }
 
@@ -104,5 +101,5 @@ module.exports = {
   view,
   edit,
   post,
-  router
+  router,
 };
--- src/repositorys/interactionrepository.js
+++ src/repositorys/interactionrepository.js
@@ -1,18 +1,17 @@
-'use strict';
 const authorisedRequest = require('../lib/authorisedrequest');
 const config = require('../config');
 
 function getInteraction(token, interactionId) {
   return authorisedRequest(token, {
     url: `${config.apiRoot}/interaction/${interactionId}/`,
-    json: true
+    json: true,
   });
 }
 
 function saveInteraction(token, interaction) {
-  let options = {
+  const options = {
     json: true,
-    body: interaction
+    body: interaction,
   };
 
   if (interaction.id && interaction.id.length > 0) {
@@ -30,5 +29,5 @@ function saveInteraction(token, interaction) {
 
 module.exports = {
   saveInteraction,
-  getInteraction
+  getInteraction,
 };
