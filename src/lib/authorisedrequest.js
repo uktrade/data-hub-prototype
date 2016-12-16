8317f49a0f82a23e74844d779c130fc44061fa50
Zac Tolley <zac@thetolleys.com>
2016-12-16 13:07:56 +0000

    Discovered a method in json parses that can intercept key value pairs and parse them, more efficient and less intrusive way of stripping script tags. Added support to call auth request with just a string

--- src/lib/authorisedrequest.js
+++ src/lib/authorisedrequest.js
@@ -2,32 +2,53 @@ const request = require('request-promise');
 const winston = require('winston');
 
 function stripScript(text) {
-  var SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+  const SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
   while (SCRIPT_REGEX.test(text)) {
-    text = text.replace(SCRIPT_REGEX, "");
+    winston.warn('Found script tag in response');
+    text = text.replace(SCRIPT_REGEX, '');
   }
   return text;
 }
 
+function isString(s) {
+  return typeof (s) === 'string' || s instanceof String;
+}
 
+// Called for each key value in a json response, strips out any script tags.
+function jsonReviver(key, value) {
+  if (isString(value)) {
+    return stripScript(value);
+  }
+  return value;
+}
+
+// Accepts either options in a kashmap or a string with a url
+// Combines the options or url with the given token to create a
+// call to the API server
+// Responses are parsed to remove any embedded XSS attempts with
+// script tags
 module.exports = (token, opts) => {
-  opts.headers = opts.headers || {};
-  opts.headers.Authorization = `Bearer ${token}`;
-  winston.debug(`Sending ${(opts.method || 'GET')} request to ${opts.url}`);
-  if (opts.method === 'POST') {
-    winston.debug(JSON.stringify(opts.body));
+  const requestOptions = {
+    json: true,
+  };
+
+  if (isString(opts)) {
+    requestOptions.url = opts;
+    requestOptions.method = 'GET';
+    requestOptions.headers = {};
+  } else {
+    requestOptions.url = opts.url;
+    requestOptions.headers = opts.headers || {};
+    requestOptions.method = opts.method || 'GET';
+
+    if (opts.body) {
+      requestOptions.body = opts.body;
+    }
   }
 
-  return new Promise((good, bad) => {
-    request(opts)
-      .then((response) => {
-        const data = JSON.parse(stripScript(response));
-        good(data);
-      })
-      .catch((error) => {
-        console.log(error);
-        bad(error);
-      });
-  });
+  requestOptions.headers.Authorization = `Bearer ${token}`;
+  winston.debug('Send authorised request: ', requestOptions);
+  requestOptions.jsonReviver = jsonReviver;
 
+  return request(requestOptions);
 };
