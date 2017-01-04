5c99fc71c510e28706602dc8bba2b7f7377a1bed
Zac Tolley <zac@thetolleys.com>
2017-01-04 12:39:06 +0000

    Updated auth module to allow kingdom through (#231)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 module.exports = function auth(req, res, next) {
   winston.debug('auth:start');
 
-  if (!req.url.startsWith('/pingdom') && !req.url.startsWith('/support') && req.url !== '/login' && req.url !== '/error' && !req.session.token) {
+  if (!req.url.startsWith('/ping.xml') && !req.url.startsWith('/support') && req.url !== '/login' && req.url !== '/error' && !req.session.token) {
     winston.debug('auth:redirect login');
     res.redirect('/login');
     return;
