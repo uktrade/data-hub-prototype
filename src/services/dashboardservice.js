6456056ee66b1cd2568126e156eaa1d91c626ac6
Reupen Shah <reupen@users.noreply.github.com>
2017-03-31 14:31:30 +0100

    [DH-71] Add test for dashboard service

--- src/services/dashboardservice.js
+++ src/services/dashboardservice.js
@@ -5,7 +5,7 @@ function mapContacts (contacts) {
   if (contacts && (typeof contacts.map) === 'function') {
     return contacts.map((contact) => {
       return {
-        url: `contact/${contact.id}`,
+        url: `/contact/${contact.id}`,
         name: `${contact.first_name} ${contact.last_name}`,
         id: contact.id,
         company: {
@@ -23,7 +23,7 @@ function mapContacts (contacts) {
 function mapInteractions (interactions) {
   if (interactions && (typeof interactions.map) === 'function') {
     return interactions.map((interaction) => {
-      const company = interaction.company || {}
+      const company = typeof interaction.company === 'object' ? interaction.company : {}
       return {
         url: `/interaction/${interaction.id}/details`,
         id: interaction.id,
